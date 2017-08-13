fc5e26a53339d2653820629c08187c204324a548
Rafael Mota <faelmota@gmail.com>
2017-08-13 09:32:09 -0300

    Forward e Redirect

--- src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -1,3 +1,4 @@
+
 package com.algaworks.brewer.config;
 
 import org.springframework.beans.BeansException;
--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -3,9 +3,11 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
 
@@ -18,15 +20,20 @@ public class CervejasController {
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		
 		if(result.hasErrors()) {
+			model.addAttribute("mensagem", "Erro no formulário"); //Só funciona com forward
 			System.out.println(">>> tem erro!");
+			return "cerveja/CadastroCerveja"; //Forward
 		}
+		
+		//Salvar no banco de dados ...
 		
 		System.out.println(">>> cadastrar!");
 		System.out.println(">>> sku:"+cerveja.getSku());
 		System.out.println(">>> nome:"+cerveja.getNome());
-		return "cerveja/CadastroCerveja";
+		attributes.addFlashAttribute("mensagem", "Cadastro realizado com sucesso"); //Atributos para redirect, cria uma seção temporaria
+		return "redirect:/cervejas/novo"; //Redirect
 	}
 }
