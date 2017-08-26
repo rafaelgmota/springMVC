2fb4be40781b7844c8ffffc31f12e56e27eb33c6
Rafael Mota <faelmota@gmail.com>
2017-08-26 19:48:48 -0300

    CidadesController

---
+++ src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -0,0 +1,34 @@
+package com.algaworks.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.algaworks.brewer.model.Cidade;
+
+@Controller
+public class CidadesController {
+	
+	@RequestMapping("/cidades/novo")
+	public String novo(Cidade cidade) { //Spring adiciona como atributro automat		
+		return "cidade/CadastroCidade";
+	}
+	
+	@RequestMapping(value = "/cidades/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Cidade cidade, BindingResult result, Model model, RedirectAttributes attributes) {
+		
+		if(result.hasErrors()) {
+			return novo(cidade);
+		}
+		
+		//Salvar no banco de dados ...
+		
+		attributes.addFlashAttribute("mensagem", "Cadastro realizado com sucesso"); //Atributos para redirect, cria uma seção temporaria
+		return "redirect:/cidades/novo"; //Redirect
+	}
+}
